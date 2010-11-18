bd40e1f66d8e9b9323b5dd7af4df5a8468c81b73
Cougar <cougar@random.ee>
2010-11-18 15:26:55 +0200

    Make it usable as Alpine/Pine display filter
    Display both diff and original message

--- bin/ripediff.sh
+++ bin/ripediff.sh
@@ -7,11 +7,17 @@
 # STDIN  - "Notification of RIPE Database changes" mail
 # STDOUT - unified diff between old and new database entry
 #
+# For Alpine (Pine) go to (M)ain Menu >> (S)etup >> (C)onfig and under
+# "Display Filters" add:
+#
+# _BEGINNING("OBJECT BELOW MODIFIED:")_ /path/where/installed/ripediff.sh
+#
 
+F0=$(mktemp ${TMPDIR:-/tmp}/ripediff-0.XXXXXXXXXX) || exit 1
 F1=$(mktemp ${TMPDIR:-/tmp}/ripediff-1.XXXXXXXXXX) || exit 1
 F2=$(mktemp ${TMPDIR:-/tmp}/ripediff-2.XXXXXXXXXX) || exit 1
 
-cat \
+tee $F0 \
 | sed 's/^      \(from\|to\)/                  \1/' \
 | awk '
 	BEGIN {
@@ -39,6 +45,12 @@ cat \
 	}
 '
 
-diff -u $F1 $F2 | less
+diff -u $F1 $F2
 
-rm $F1 $F2
+echo
+echo "---------- ORIGINAL MESSAGE ----------"
+echo
+
+cat $F0
+
+rm $F0 $F1 $F2
